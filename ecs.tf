71554ad3b60b79da5ce011a312d0b5bceb9c56cc
Diego Ferreira <katesclau@gmail.com>
2018-05-02 15:48:48 -0300

    Updates ecs.tf to allow remote module reference

--- ecs.tf
+++ ecs.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = "eu-west-1"
-}
-
 module "ecs" {
   source = "modules/ecs"
 
@@ -15,14 +11,9 @@ module "ecs" {
   max_size             = "${var.max_size}"
   min_size             = "${var.min_size}"
   desired_capacity     = "${var.desired_capacity}"
-  key_name             = "${aws_key_pair.ecs.key_name}"
+  key_name             = "${var.key_name}"
   instance_type        = "${var.instance_type}"
   ecs_aws_ami          = "${var.ecs_aws_ami}"
-}
-
-resource "aws_key_pair" "ecs" {
-  key_name   = "ecs-key-${var.environment}"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCtMljjj0Ccxux5Mssqraa/iHHxheW+m0Rh17fbd8t365y9EwBn00DN/0PjdU2CK6bjxwy8BNGXWoUXiSDDtGqRupH6e9J012yE5kxhpXnnkIcLGjkAiflDBVV4sXS4b3a2LSXL5Dyb93N2GdnJ03FJM4qDJ8lfDQxb38eYHytZkmxW14xLoyW5Hbyr3SXhdHC2/ecdp5nLNRwRWiW6g9OA6jTQ3LgeOZoM6dK4ltJUQOakKjiHsE+jvmO0hJYQN7+5gYOw0HHsM+zmATvSipAWzoWBWcmBxAbcdW0R0KvCwjylCyRVbRMRbSZ/c4idZbFLZXRb7ZJkqNJuy99+ld41 ecs@aws.fake"
 }
 
 variable "vpc_cidr" {}
@@ -32,6 +23,7 @@ variable "min_size" {}
 variable "desired_capacity" {}
 variable "instance_type" {}
 variable "ecs_aws_ami" {}
+variable "key_name" {}
 
 variable "private_subnet_cidrs" {
   type = "list"
